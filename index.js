a8f4618b1ed8017a761ea215bdecae2e3f7d3b2b
Alif Dzikri <d.ziunincode69@gmail.com>
2024-04-28 17:21:30 +0700

    initialize

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-const port = process.env.APP_PORT;
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
