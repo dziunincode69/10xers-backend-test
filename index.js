af969e1675c20b6b22bea3e24e461730e80222fa
Alif Dzikri <d.ziunincode69@gmail.com>
2024-04-28 17:17:59 +0700

    initialize

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.APP_PORT;
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
