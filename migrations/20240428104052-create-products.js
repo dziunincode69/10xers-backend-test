059e2210c23067ec99adcef802289f669e7e203c
Alif Dzikri <d.ziunincode69@gmail.com>
2024-04-28 17:48:35 +0700

    init migration

---
+++ migrations/20240428104052-create-products.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Products", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      user_id: {
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        type: Sequelize.INTEGER,
+      },
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
